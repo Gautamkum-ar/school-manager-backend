b9b2654c95440c6235a1c2a81822261db2f027c2
GAUTAM KUMAR <gkvc9696@gmail.com>
2023-10-20 19:09:03 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import "./databse/initial-db.js";
+
+const app = express();
+dotenv.config();
+
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use("/v1/api", userRouter);
+
+app.use("*", (req, res) => {
+	res.status(404).json({
+		success: false,
+		message: "Route not found",
+		method: `${req.method} ${req.url} is not allowed`,
+	});
+});
+
+app.use((err, req, res, next) => {
+	return res.status(err.statusCode || 500).json({
+		statusCode: err.statusCode || 500,
+		success: false,
+		message: err.message,
+	});
+});
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+	console.log(`App started on port ${port}`);
+});
